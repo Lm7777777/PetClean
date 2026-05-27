67c54410461e45597f2814c24e3641c8770c5aeb
Lm7777777 <e.iet.miguel.gaviria982@cali.edu.co>
2026-05-27 09:12:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mensaje de bienvenida
+window.onload = function() {
+    console.log("Bienvenido a PetClean 🐾");
+}
+
+// Ejemplo: alerta al hacer clic en promo
+document.querySelector(".promo").addEventListener("click", () => {
+    alert("¡Aprovecha nuestro descuento del 25%!");
+});
